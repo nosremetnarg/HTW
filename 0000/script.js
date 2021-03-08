5e89ce5a22afeba8816efe0050ee084e9a74b401
Grant Emerson <grant.alex.emerson@gmail.com>
2021-03-08 13:11:02 -0600

    htw animation complete

---
+++ 0000/script.js
@@ -0,0 +1,12 @@
+const logo2 = document.querySelectorAll('#huntTheWitchText path');
+// console.log(logo.length);
+// for (let i = 0; i < logo.length; i++) {
+//     // console.log(`Letter ${i + 1} is ${logo[i].getTotalLength()}`);
+// }
+
+// const logo2 = document.querySelectorAll('#logo2 path');
+// // console.log(logo2.length);
+
+for (let i = 0; i < logo2.length; i++) {
+    console.log(`Letter ${i + 1} is ${logo2[i].getTotalLength()}`);
+}
