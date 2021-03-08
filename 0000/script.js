5b8df1425e54a96920703a76a30637df12128bdc
Grant Emerson <grant.alex.emerson@gmail.com>
2021-03-08 14:02:59 -0600

    7011 number reveal working

--- 0000/script.js
+++ 0000/script.js
@@ -1,12 +1,18 @@
 const logo2 = document.querySelectorAll('#huntTheWitchText path');
-// console.log(logo.length);
-// for (let i = 0; i < logo.length; i++) {
-//     // console.log(`Letter ${i + 1} is ${logo[i].getTotalLength()}`);
-// }
-
-// const logo2 = document.querySelectorAll('#logo2 path');
-// // console.log(logo2.length);
+const text7011 = document.querySelectorAll(".text7011");
+const body = document.querySelector('.body');
 
 for (let i = 0; i < logo2.length; i++) {
     console.log(`Letter ${i + 1} is ${logo2[i].getTotalLength()}`);
 }
+
+
+
+body.addEventListener('click', function() {
+    console.log("clicked")
+    body.classList.remove(".body");
+    body.style.background = "black";
+    document.getElementById("text7011").classList.remove("text7011");
+    document.getElementById("text7011").classList.add("text7011Black");
+    document.getElementById("huntTheWitchText").style.opacity = 0;
+})
