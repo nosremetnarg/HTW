7b35388a6ca0b9b74197fe2daea2aead13eca630
Grant Emerson <grant.alex.emerson@gmail.com>
2021-03-09 13:00:39 -0600

    added crickets sound

--- 7011/script.js
+++ 7011/script.js
@@ -1,8 +1,17 @@
-const audio = new Audio("BottomOfTheRiver-CricketsIntro.mp3");
+const audio = new Audio("../assets/BottomOfTheRiver-CricketsIntro.mp3");
 const body = document.getElementById("body");
 
 let isPlaying = false;
 
+function playAudio() {
+    audio.play();
+    audio.loop = true
+    isPlaying = true;
+  }
+  function stopAudio() {
+    audio.pause();
+    isPlaying = false;
+  }
 
 document.addEventListener("click", function () {
     if (!isPlaying) {
