448e2640da5953c90d973365ba470093c13072ed
Grant Emerson <grant.alex.emerson@gmail.com>
2021-04-24 13:21:39 -0500

    added in mob sound

--- Step2/script.js
+++ Step2/script.js
@@ -2,6 +2,10 @@ const wordEl = document.getElementById("word");
 const wrongLettersEl = document.querySelector(".wrong-letters");
 const finalMessage = document.getElementById("final-message");
 const notification = document.getElementById("notification-container");
+const popup = document.getElementById("popup-container");
+const mobAudio = new Audio(
+  "https://static1.squarespace.com/static/60046592044342497f59ed79/606db93b02bfcf3eb6224e19/60803a5b9d99fb68ef7de910/1619016337096/Hunt+The+Witch+-+Mob.mp3"
+);
 const secretWord = "iwillneverdie";
 
 const correctLetters = [];
@@ -36,19 +40,7 @@ function updateWrongLettersEl() {
     ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
       `;
-  // Display body parts
-//   figureParts.forEach((part, index) => {
-//     const errors = wrongLetters.length;
-
-//     if (index < errors) {
-//       part.style.display = "block";
-//     } else {
-//       part.style.display = "none";
-//     }
-//   });
-
-  // Check if lostt
-
+  // Check if player lost
   if (wrongLetters.length === 5) {
     finalMessage.innerText = "You Lost";
     popup.style.display = "flex";
@@ -61,6 +53,12 @@ function showNotification() {
   setTimeout(() => {
     notification.classList.remove("show");
   }, 2000);
+}
+let wrongLetterCount = 0;
+// Play Sounds for each wrong Answer
+function wrongAnswerSounds() {
+  if (wrongLetterCount === 1) {
+  }
 }
 
 // Keydown letter press
@@ -78,14 +76,35 @@ window.addEventListener("keydown", (e) => {
       }
     } else {
       if (!wrongLetters.includes(letter)) {
+        wrongLetterCount++;
         wrongLetters.push(letter);
-
         updateWrongLettersEl();
       } else {
         showNotification();
       }
     }
   }
+  console.log(`${wrongLetterCount} wrong guesses`);
 });
+let isPlaying = false;
+function playAudio() {
+  mobAudio.play();
+  mobAudio.loop = true;
+  isPlaying = true;
+}
+function stopAudio() {
+  mobAudio.pause();
+  isPlaying = false;
+}
+
+document.addEventListener("click", function () {
+    if (!isPlaying) {
+      playAudio();
+      mobAudio.volume = 0.2;
+    } else {
+      stopAudio();
+    }
+    console.log("Do you hear the mob?");
+  });
 
 displayWord();
